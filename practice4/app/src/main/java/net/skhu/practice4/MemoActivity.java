cfa1b4cb927d855c3dee703076c5678ffad3b3f2
sumin <cotack@naver.com>
2018-09-28 14:12:55 +0900

    18/09/28

---
+++ practice4/app/src/main/java/net/skhu/practice4/MemoActivity.java
@@ -0,0 +1,43 @@
+package net.skhu.practice4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+public class MemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+
+
+        Button button = (Button) findViewById(R.id.btnSave);
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText editText_title = (EditText) findViewById(R.id.editText_title);
+                String title = editText_title.getText().toString();
+                if (isEmptyOrWhiteSpace(title)) {
+                    editText_title.setError("제목을 입력하세요.");
+                }
+
+                EditText editText_content = (EditText) findViewById(R.id.editText_content);
+                String content = editText_content.getText().toString();
+                if (isEmptyOrWhiteSpace()) {
+                    editText_content.setError("내용을 입력하세요.");
+                }
+
+                String msg = "저장 성공: " + title;
+                Toast.makeText(MemoActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
+        };
+        button.setOnClickListener(listener);
+    }
+    static boolean isEmptyOrWhiteSpace(String s){
+        if(s == null) return true;
+        return s.toString().trim().length() == 0;
+    }
+}
