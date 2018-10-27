e7d6ee80e4b39a89926fa24a5299be80a90f0944
sumin <cotack@naver.com>
2018-10-28 00:03:42 +0900

    android 18/10/28

--- practice5/app/src/main/java/net/skhu/practice5/SpinnersActivity.java
+++ practice5/app/src/main/java/net/skhu/practice5/SpinnersActivity.java
@@ -2,6 +2,11 @@ package net.skhu.practice5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class SpinnersActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class SpinnersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinners);
+
+        String[] stringArray = { "집주소", "직장주소", "기타"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner_addressType);
+        spinner.setAdapter(adapter);
+
+        View.OnClickListener listener1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner spinner1 = (Spinner)findViewById(R.id.spinner_phoneType);
+                int index1 = spinner1.getSelectedItemPosition();
+                String text1 = spinner1.getSelectedItem().toString();
+
+                Spinner spinner2 = (Spinner)findViewById(R.id.spinner_addressType);
+                int index2 = spinner2.getSelectedItemPosition();
+                String text2  = spinner2.getSelectedItem().toString();
+
+                String s = String.format("전화:%s(%d) 주소:%s(%d)", text1, index1, text2, index2);
+                Toast.makeText(SpinnersActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+        };
+        Button button = (Button)findViewById(R.id.btnSave);
+        button.setOnClickListener(listener1);
     }
 }
