cc881a8ef7e7c3d79581dae66b451be1a4b37aa7
sumin <cotack@naver.com>
2018-10-29 16:04:00 +0900

    18/10/29 android2

---
+++ practice42/app/src/main/java/net/skhu/practice4/MainActivity.java
@@ -0,0 +1,36 @@
+package net.skhu.practice4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.content.Intent;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_signUp){
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if(id == R.id.action_memo){
+            Intent intent = new Intent(this, MemoActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return onOptionsItemSelected(item);
+    }
+}
