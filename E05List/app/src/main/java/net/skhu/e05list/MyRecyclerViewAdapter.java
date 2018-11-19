a625e1b9ff628d330fd81b398b167d9b3433bf9c
sumin <cotack@naver.com>
2018-11-19 14:30:07 +0900

    android 18/11/19

---
+++ E05List/app/src/main/java/net/skhu/e05list/MyRecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package net.skhu.e05list;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView textView1, textView2;
+
+        public ViewHolder(View view){
+            super(view);
+            textView1 = view.findViewById(R.id.textView1);
+            textView2 = view.findViewById(R.id.textView2);
+            view.setOnClickListener(this);
+        }
+
+        public void setData(){
+            Item item = arrayList.get(getAdapterPosition());
+            textView1.setText(item.getTitle());
+            textView2.setText(item.getCreateTimeFormatted());
+        }
+
+        public void onClick(View view){
+            Item item = arrayList.get(super.getAdapterPosition());
+            String s = String.format("index: %d,    title: %s", super.getAdapterPosition(), item.getTitle());
+            Toast.makeText(view.getContext(), s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    LayoutInflater layoutInflater;
+    ArrayList<Item> arrayList;
+
+    public MyRecyclerViewAdapter(Context context, ArrayList<Item> arrayList){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.arrayList = arrayList;
+    }
+
+    public int getItemCount(){
+        return arrayList.size();
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
+        View view = layoutInflater.inflate(R.layout.item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    public void onBindViewHolder(final ViewHolder viewHolder, final int index){
+        viewHolder.setData();
+    }
+}
